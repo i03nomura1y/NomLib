ba68162a15e51cfdd4890a77e320f916aaf83705
Yusuke Nomura <i03nomura1y@gmail.com>
2012-12-16 22:49:25 +0900

    update regex

--- regex.cpp
+++ regex.cpp
@@ -10,131 +10,133 @@
 
 namespace nl{
 
-  RegEx::RegEx(const std::string &rxstr) : m_reg(new regex_t), m_rx_str(rxstr), str(),idx_list(){
-	char errbuf[100];
-	// regcomp の最後の引数
-	//   REG_EXTENDED : POSIX拡張正規表現を使用。指定なしなら POSIX 標準正規表現が使われる
-	//   REG_ICASE    : 大文字小文字の違いを無視
-	//   REG_NOSUB    : regexec で パラメータ nmatch, pmatch が無視される
-	//   REG_NEWLINE  : 全ての文字にマッチするオペレータに改行をマッチさせない
-	int ret = regcomp(m_reg, rxstr.c_str(), REG_EXTENDED | REG_NEWLINE );
-	if(ret != 0){
-	  regerror(ret, m_reg, errbuf, sizeof errbuf);
-	  ERRP("regcomp error:" << errbuf);
-	  ERRP("  rx str: '" << rxstr << "'");
-	  delete m_reg;
-	}
-	valid = (ret == 0); // コンパイルに成功したら true
-	idx_list.clear();
-  }
+    RegEx::RegEx(const std::string &rxstr) : valid(false), str(),idx_list(){
+        compile(rxstr);
+    }
 
-  // コピーコンストラクタ
-  RegEx::RegEx(const RegEx &obj)
-	: valid(obj.valid),
-	  m_reg(obj.m_reg),
-	  m_rx_str(obj.m_rx_str),
-	  str(obj.str),
-	  idx_list(obj.idx_list) {
-	obj.m_reg = NULL;
-  }
+    // コピーコンストラクタ
+    RegEx::RegEx(const RegEx &obj)
+        : valid(false),
+          str(obj.str),
+          idx_list(obj.idx_list) {
+        compile(obj.m_rx_str);
+    }
+    // 代入演算子
+    RegEx &RegEx::operator=(RegEx &obj){
+        compile(obj.m_rx_str);
+        str = obj.str;
+        idx_list = obj.idx_list;
+        return *this;
+    }
+
+    RegEx::~RegEx(){
+        idx_list.clear();
+        if(valid) regfree(&m_reg);
+    }
   
-  RegEx::~RegEx(){
-	idx_list.clear();
-	if(m_reg != NULL){
-	  if(valid) regfree(m_reg);
-	  delete m_reg;
-	}
-  }
-  
-  // 代入演算子
-  RegEx &RegEx::operator=(RegEx &obj){
-	swap(obj);
-	return *this;
-  }
+    void RegEx::swap(RegEx &obj){
+        using std::swap;
+        swap(valid   , obj.valid   );
+        swap(m_reg   , obj.m_reg   );
+        swap(m_rx_str, obj.m_rx_str);
+        swap(str     , obj.str     );
+        swap(idx_list, obj.idx_list);
+    }
 
-  void RegEx::swap(RegEx &obj){
-	using std::swap;
-	swap(valid   , obj.valid   );
-	swap(m_reg   , obj.m_reg   );
-	swap(m_rx_str, obj.m_rx_str);
-	swap(str     , obj.str     );
-	swap(idx_list, obj.idx_list);
-  }
-
-
-  bool RegEx::match(const std::string &str_){
-	if(m_reg == NULL){ ERRP("reg is NULL"); return false; }
+    // 正規表現文字列をコンパイルする
+    // @param rxstr 正規表現 文字列
+    bool RegEx::compile(const std::string &rxstr){
+        if(valid) regfree(&m_reg);
+        // regcomp の最後の引数
+        //   REG_EXTENDED : POSIX拡張正規表現を使用。指定なしなら POSIX 標準正規表現が使われる
+        //   REG_ICASE    : 大文字小文字の違いを無視
+        //   REG_NOSUB    : regexec で パラメータ nmatch, pmatch が無視される
+        //   REG_NEWLINE  : 全ての文字にマッチするオペレータに改行をマッチさせない
+        int ret = regcomp(&m_reg, rxstr.c_str(), REG_EXTENDED | REG_NEWLINE );
+        valid = (ret == 0); // コンパイルに成功したら true
+        m_rx_str = rxstr;
+        if( !valid ){
+            char errbuf[100];
+            regerror(ret, &m_reg, errbuf, sizeof errbuf);
+            ERRP("regcomp error:" << errbuf);
+            ERRP("  rx str: '" << rxstr << "'");
+        }
+        return valid;
+    }
+        
+    bool RegEx::match(const std::string &str_){
+        if( !valid ){ ERRP("invalid regex"); return false; }
 	
-	size_t nmatch = 100;
-	regmatch_t pmatch[nmatch];
-	idx_list.clear();
-	str = str_;
-	// regexec の最後の引数
-	//   REG_NOTBOL : 行頭にマッチするオペレータは必ずマッチに失敗する
-	//                (複数行文字列の先頭を行頭として解釈させない場合に用いる)
-	//   REG_NOTEOL : 行末にマッチするオペレータは必ずマッチに失敗する
-	if(valid && (regexec(m_reg, str_.c_str() , nmatch, pmatch, 0) == REG_NOERROR) ){
-	  for (unsigned int i = 0; i < nmatch && pmatch[i].rm_so>=0; i++) {
-		int so = (int)pmatch[i].rm_so;
-		int eo = (int)pmatch[i].rm_eo;
-		//std::cout << i << " " << so << " " << eo << " " << (eo-so) << std::endl;
-		idx_list.push_back( std::pair<int,int>( so, eo-so ) );
-	  }
-	  //std::cout << "size " << idx_list.size() << std::endl;
-	  return true;
-	}
-	return false;
-  }
-  /// 前回の実行によりマッチした部分文字列の idx 番目のものを返す
-  std::string RegEx::get(unsigned int idx) const{
-	if( idx>=idx_list.size() ){
-	  ERRP("size "<< idx_list.size() << "  arg " << idx);
-	  throw std::out_of_range("in RegEx::get(int)");
-	}
-	//std::cout << idx_list[idx].first << "," << idx_list[idx].second << std::endl;
-	//std::cout << str << "," << (str.substr(0,1)) << std::endl;
-	return str.substr( idx_list[idx].first, idx_list[idx].second );
-  }
-  int RegEx::spos(unsigned int idx) const{
-	if( idx>=idx_list.size() ){
-	  ERRP("size "<< idx_list.size() << "  arg " << idx);
-	  throw std::out_of_range("in RegEx::spos(int)");
-	}
-	return idx_list[idx].first;
-  }
-  int RegEx::epos(unsigned int idx) const{
-	if( idx>=idx_list.size() ){
-	  ERRP("size "<< idx_list.size() << "  arg " << idx);
-	  throw std::out_of_range("in RegEx::epos(int)");
-	}
-	return ( idx_list[idx].first + idx_list[idx].second );
-  }
+        size_t nmatch = 100;
+        regmatch_t pmatch[nmatch];
+        idx_list.clear();
+        str = str_;
+        // regexec の最後の引数
+        //   REG_NOTBOL : 行頭にマッチするオペレータは必ずマッチに失敗する
+        //                (複数行文字列の先頭を行頭として解釈させない場合に用いる)
+        //   REG_NOTEOL : 行末にマッチするオペレータは必ずマッチに失敗する
+        if(valid && (regexec(&m_reg, str_.c_str() , nmatch, pmatch, 0) == REG_NOERROR) ){
+            for (unsigned int i = 0; i < nmatch && pmatch[i].rm_so>=0; i++) {
+                int so = (int)pmatch[i].rm_so;
+                int eo = (int)pmatch[i].rm_eo;
+                //std::cout << i << " " << so << " " << eo << " " << (eo-so) << std::endl;
+                idx_list.push_back( std::pair<int,int>( so, eo-so ) );
+            }
+            //std::cout << "size " << idx_list.size() << std::endl;
+            return true;
+        }
+        return false;
+    }
+    /// 前回の実行によりマッチした部分文字列の idx 番目のものを返す
+    std::string RegEx::get(unsigned int idx) const{
+        if( idx>=idx_list.size() ){
+            ERRP("size "<< idx_list.size() << "  arg " << idx);
+            throw std::out_of_range("in RegEx::get(int)");
+        }
+        //std::cout << idx_list[idx].first << "," << idx_list[idx].second << std::endl;
+        //std::cout << str << "," << (str.substr(0,1)) << std::endl;
+        return str.substr( idx_list[idx].first, idx_list[idx].second );
+    }
+    int RegEx::spos(unsigned int idx) const{
+        if( idx>=idx_list.size() ){
+            ERRP("size "<< idx_list.size() << "  arg " << idx);
+            throw std::out_of_range("in RegEx::spos(int)");
+        }
+        return idx_list[idx].first;
+    }
+    int RegEx::epos(unsigned int idx) const{
+        if( idx>=idx_list.size() ){
+            ERRP("size "<< idx_list.size() << "  arg " << idx);
+            throw std::out_of_range("in RegEx::epos(int)");
+        }
+        return ( idx_list[idx].first + idx_list[idx].second );
+    }
 
-  /// 置換
-  // src 内のマッチした部分文字列を repl で置き換える
-  std::string RegEx::replace(const std::string &src, const std::string &repl, int idx){
-	std::stringstream ss;
-	unsigned int i;
-	for(i = 0; i<src.length() && match(&src[i]); i += epos(idx)){
-	  //DBGP("'" << ss.str() << "' <- '" << &src[i] << "'");
-	  //DBGP("match  " << (re.spos(idx)) << " , " << (re.epos(idx)));
-	  //DBGP("'" << src.substr(i, re.spos(idx)) << "'");
-	  ss << src.substr(i, spos(idx)) << repl;
-	}
-	if(i<src.length()) ss << src.substr(i);
-	//DBGP("'" << ss.str() << "' <- '" << &str[i] << "'");
-	return ss.str();
-  }
+    /// 置換
+    // src 内のマッチした部分文字列を repl で置き換える
+    std::string RegEx::replace(const std::string &src, const std::string &repl, int idx){
+        std::stringstream ss;
+        unsigned int i;
+        for(i = 0; i<src.length() && match(&src[i]); i += epos(idx)){
+            //DBGP("'" << ss.str() << "' <- '" << &src[i] << "'");
+            //DBGP("match  " << (re.spos(idx)) << " , " << (re.epos(idx)));
+            //DBGP("'" << src.substr(i, re.spos(idx)) << "'");
+            ss << src.substr(i, spos(idx)) << repl;
+        }
+        if(i<src.length()) ss << src.substr(i);
+        //DBGP("'" << ss.str() << "' <- '" << &str[i] << "'");
+        return ss.str();
+    }
   
-  /*
-  // sample: \$ -> $
-  std::string RegEx::replace_dollar( const std::string &str ){
+    /*
+    // sample: \$ -> $
+    std::string RegEx::replace_dollar( const std::string &str ){
 	RegEx re("\\\\.");
 	std::stringstream ss;
 	unsigned int i;
 	for(i = 0; i<str.length() && re.match(&str[i]); i += re.epos(0)){
-	  if(re.get(0)[1] == '$') ss << str.substr(i, re.spos(0)) << "$";
-	  else                    ss << str.substr(i, re.epos(0));
+    if(re.get(0)[1] == '$') ss << str.substr(i, re.spos(0)) << "$";
+    else                    ss << str.substr(i, re.epos(0));
 	}
 	if(i<str.length()) ss << str.substr(i);
 	return ss.str();
@@ -144,75 +146,42 @@ namespace nl{
 };
 
 #ifdef TEST
-// test
 // $ make TEST=regex
 #include <iostream>
 using namespace std;
 using nl::RegEx;
 
-void test(const string &rx_str, const string &str1, const string &str2){
-  RegEx re(rx_str);
+void test(const string &rx_str, const string &str1){
+    RegEx re(rx_str);
 
-  if( !re.isValid() ){ cerr << "error: invalid RegEx" << endl; return; }
+    if( !re.isValid() ){ cerr << "error: invalid RegEx" << endl; return; }
   
-  cout << " ================================================" << endl;
-  cout << "rx  : " << re.rx_str() << endl;
-  cout << "str : '" << str1 << "'" << endl;
-  if( re.match(str1) ){
-	for(unsigned int i=0; i<re.length(); i++){
-	  cout << " match[" << i << "] " << re.get(i) << " " << re.spos(i) << endl;
-	}
-  }else{
-	cout << " no match" << endl;
-  }
-
-  cout << "str : '" << str2 << "'" << endl;
-  if( re.match(str2) ){
-	for(unsigned int i=0; i<re.length(); i++){
-	  cout << " match[" << i << "] " << re.get(i) << " " << re.spos(i) << endl;
-	}
-  }else{
-	cout << " no match" << endl;
-  }
-}
-
-std::string my_replace(const std::string &src){
-  RegEx re("(a.)");
-  std::stringstream ss;
-  unsigned int i;
-  for(i = 0; i<src.length() && re.match(&src[i]); i += re.epos(0)){
-	if(re.get(0) == "ab") ss << src.substr(i, re.spos(0)) << "b";
-	else                  ss << src.substr(i, re.epos(0));
-  }
-  if(i<src.length()) ss << src.substr(i);
-  return ss.str();
-}
-
-
-void test_replace(const std::string &re_str, const std::string &str, const std::string &repl, int idx = 0){
-  RegEx re(re_str);
-  DBGP("'" << re.replace(str, repl, idx) << "' <- '" << str << "'");
+    cout << " ================================================" << endl;
+    cout << "rx  : " << re.rx_str() << endl;
+    cout << "str : '" << str1 << "'" << endl;
+    if( re.match(str1) ){
+        for(unsigned int i=0; i<re.length(); i++){
+            cout << " [" << i << "] " << re.get(i) << " (" << re.spos(i) << "-" << re.epos(i) << ")" << endl;
+        }
+    }else{
+        cout << " no match" << endl;
+    }
 }
 
 int main(){
-  test_replace("hello", "hello,world!!", "good night");
+    // match のテスト
+    test("hello", "hello, world!!");
+    test("(\\w*), (\\w*)", "hello, world!!");
 
-  DBGP("" << my_replace("zzzab"));
-  DBGP("" << my_replace("zzzaab"));
-  DBGP("" << my_replace("zzzaaab"));
-
-  // [\x20-\x7F]  0x20 - 0x7F の一文字にマッチ。 '\' はひとつだけでOK
-
-  /*
-  test("([[:digit:]]+), (([[:digit:]]+), ([[:digit:]]+))", 
-	   "123, 456, 789", "Hello");
-
-  test("^hello", 
-	   "hello", "hello, world!");
-
-  test("(日本語)?のテスト",
-	   "日本語のテスト", "英語のテスト");
-  //*/
-  return 0;
+    // replace のテスト
+    cout << " ================================================" << endl;
+    cout << "replace_test:" << endl;
+    std::string in = "Good morning! morning!";
+    RegEx re("morning");
+    cout << in << endl;
+    // re にマッチした部分を置き換える。
+    cout << " ->" << re.replace(in, "evening") << endl;
+    
+    return 0;
 }
 #endif
--- regex.h
+++ regex.h
@@ -1,6 +1,5 @@
 // -*- mode: cpp -*-
 // regex.h の c++ 版
-//  regex_t はスマートポインタで扱う。
 // -lregex
 #ifndef __NOMLIB_REGEX_H__
 #define __NOMLIB_REGEX_H__
@@ -10,51 +9,55 @@
 #include <vector>
 
 namespace nl{
-  class RegEx{
-  public:
-	// 正規表現文字列をコンパイルする
-	// @param rxstr 正規表現 文字列
-	explicit RegEx(const std::string &rxstr);
-	virtual ~RegEx();
-	RegEx(const RegEx &obj); // コピーコンストラクタ
-	RegEx &operator=(RegEx &obj);  // 代入演算子
-	void swap(RegEx &obj);
+    class RegEx{
+    public:
+        // 正規表現文字列をコンパイルする
+        // @param rxstr 正規表現 文字列
+        // [\x20-\x7F]  0x20 - 0x7F の一文字にマッチ。 '\' はひとつだけでOK
+        explicit RegEx(const std::string &rxstr);
+        virtual ~RegEx();
+        RegEx(const RegEx &obj); // コピーコンストラクタ
+        RegEx &operator=(RegEx &obj); // 代入演算子
+        void swap(RegEx &obj);
+    public:
+        // 正規表現文字列をコンパイルする
+        // @param rxstr 正規表現 文字列
+        bool compile(const std::string &rxstr);
+        // コンパイル時に指定された正規表現文字列を返す
+        std::string rx_str() const{ return m_rx_str; };
+        // 正規表現を正常にコンパイルできているか？
+        bool isValid() const { return valid; }
+	
+        /// 正規表現にマッチする?
+        // @return  マッチしたら true
+        bool match(const std::string &str_);
+        
+        // マッチした部分文字列の個数
+        unsigned int length() const{ return idx_list.size(); }
+	
+        /// 前回の実行によりマッチした部分文字列の idx 番目のものを返す
+        std::string get(unsigned int idx) const; // 部分文字列
+        int spos(unsigned int idx) const; // 開始インデクス
+        int epos(unsigned int idx) const;   // 終了インデクス
 
-	// コンストラクタに渡された文字列を返す
-	std::string rx_str() const{ return m_rx_str; };
-	// 正規表現を正常にコンパイルできたか？
-	bool isValid() const { return valid; }
-	
-	/// 正規表現にマッチする?
-	// @return  マッチしたら true
-	bool match(const std::string &str_);
-	/// 前回の実行によりマッチした部分文字列の idx 番目のものを返す
-	std::string get(unsigned int idx) const; // 部分文字列
-	//int getStartPos(int idx) const; // 開始インデクス
-	//int getEndPos(int idx) const;   // 終了インデクス
-	int spos(unsigned int idx) const; // 開始インデクス
-	int epos(unsigned int idx) const;   // 終了インデクス
-	// 部分文字列の個数
-	unsigned int length() const{ return idx_list.size(); }
-	
-	/// 置換
-	// src 内のマッチした部分文字列を repl で置き換える
-	// @param src  元の文字列
-	// @param repl 置き換え後の部分文字列
-	// @param idx  何番目の match を置き換えるか
-	// @return 置き換え後の文字列
-	std::string replace(const std::string &src, const std::string &repl, int idx = 0);
+        /// 置換
+        // src 内のマッチした部分文字列を repl で置き換える
+        // @param src  元の文字列
+        // @param repl 置き換え後の部分文字列
+        // @param idx  何番目の match を置き換えるか
+        // @return 置き換え後の文字列
+        std::string replace(const std::string &src, const std::string &repl, int idx = 0);
 
-  private:
-	bool valid;
-	mutable regex_t *m_reg; // smart pointer として使う。
-	std::string m_rx_str; // コンストラクタに渡された文字列
-	// 前回 match()実行したときの文字列
-	std::string str;
-	// 前回の match()実行によりマッチした部分文字列の(インデクス,長さ) ただし最大100個
-	std::vector<std::pair<int,int> > idx_list;
+    private:
+        bool valid;
+        regex_t m_reg;
+        std::string m_rx_str; // コンストラクタに渡された文字列
+        // 前回 match()実行したときの文字列
+        std::string str;
+        // 前回の match()実行によりマッチした部分文字列の(インデクス,長さ) ただし最大100個
+        std::vector<std::pair<int,int> > idx_list;
 	
-  };
+    };
 }; // namespace nl
 
 #endif

e626fb8862e08d3faacc6047d47ddcc0e5813a0c
Yusuke Nomura <i03nomura1y@gmail.com>
2012-12-18 23:38:33 +0900

    rename lexer_basic -> lexer

--- lexer.cpp
+++ lexer.cpp
@@ -1,30 +1,174 @@
 // -*- mode: cpp -*-
 #include "lexer.h"
 
-#include <algorithm>
 #include "util.h"
 
 namespace nl{
+    static RegEx re_lastEscape("^(((\\\\.)|[^\\\\])*)\\\\$");
+
+    Lexer::Lexer() : is(NULL), ifs(NULL), ss(NULL){ init(); }
+    Lexer::Lexer(const std::string &file_name_)
+        : is(NULL), ifs(NULL), ss(NULL){
+        init();
+        setSourceFile(file_name_);
+    }
+    // コピーコンストラクタ 元のやつは壊す
+    Lexer::Lexer(const Lexer &obj) : is(NULL), ifs(NULL), ss(NULL){
+        str       = obj.str      ;
+        line_no   = obj.line_no  ;
+        idx 	  = obj.idx 	 ;
+        pre_idx   = obj.pre_idx  ;
+        file_name = obj.file_name;
+        swap(ifs, obj.ifs);
+        swap(ss , obj.ss );
+        is            = obj.is;
+        line_offset   = obj.line_offset;
+        col_offset    = obj.col_offset;
+        start_col_pos = obj.start_col_pos;
+    }
+
+    Lexer::~Lexer(){
+        if( ifs != NULL ) delete ifs;
+        if( ss  != NULL ) delete ss;
+        is = NULL;
+    }
+
+    void Lexer::init(){
+        if( ifs != NULL ) delete ifs;
+        if( ss  != NULL ) delete ss;
+        ifs = NULL;
+        ss  = NULL;
+        file_name = "";
+        is = NULL;
+        reg_str = "";
+
+        str     = "";
+        line_no = idx = pre_idx =  0;
+        line_offset = col_offset = start_col_pos = 0;
+    }
     
-    Lexer::~Lexer(){}
+    // 入力元として、ファイルを設定
+    void Lexer::setSourceFile(const std::string &file_name_){
+        init();
+        file_name     = file_name_;
+        ifs = new std::ifstream( file_name.c_str(), std::ios::in | std::ios::binary );
+        if(!ifs->is_open()){ ERRP("error: cannot open file " << file_name); delete ifs; }
+        else is = ifs;
+    }
+    // 入力元として、文字列を設定
+    void Lexer::setSourceText(const std::string &content_){
+        init();
+        ss = new std::stringstream();
+        ss->str(content_);
+        is = ss;
+    }    
+    // 入力元の追加情報のセット
+    void Lexer::setSourceInfo(const std::string &file_name_,
+                              const int line_offs,
+                              const int col_offs,
+                              const int start_col_pos_){
+        if( file_name_.length() > 0 )  file_name = file_name_;
+        line_offset   = line_offs;
+        col_offset    = col_offs;
+        start_col_pos = start_col_pos_;
+    }
+
+    // トークンをひとつ切り出す。
+    bool Lexer::getToken(LexRule &tkn){
+        if( !updateString()   ) return false;
+        if( !tkn.match( &str[idx] ) ) return false;
+        incCursor( tkn.get(0).length() );
+        reg_str = tkn.str();
+        return true;
+    }
+    // @return マッチした: true / マッチしなかった: false
+    bool Lexer::getToken(const std::string &tkn_){
+        if( !updateString()   ) return false;
+        int t_len = tkn_.length();        // token length
+        int s_len = str.length() - idx;   // 残りstr length
+        if( s_len < t_len ) return false; //  (残りstr < token) -> false
+        for(int i=0;i<t_len;i++)
+            if( tkn_[i] != str[i+idx] ) return false;
+        incCursor( tkn_.length() );
+        reg_str = tkn_;
+        return true;
+    }
 
 #if 0
-    LexRule *Lexer::getToken(){
-        return (reg_tkn = get());
-#if 0
-        // token.type が skip_types に含まれている間ループ
-        while( (reg_tkn = get()) &&
-               std::find( skip_types.begin(), skip_types.end(), reg_tkn->type) != skip_types.end());
-        return reg_tkn;
-#endif
+    // 最初にマッチしたやつを返す
+    LexRule *Lexer::getToken(std::list<LexRule> &lst){
+        if( !updateString()   ) return (LexRule*)NULL;
+        for( std::list<LexRule>::iterator ite = lst.begin(); ite != lst.end(); ++ite){
+            if( getToken(*ite) ) return &(*ite);
+        }
+        return NULL;
     }
 #endif
+
+    // 前回の get() を無かったことにする
+    void Lexer::unget(){ idx = pre_idx; }
+
+    // 読み取り中の行の残り部分を返す。idx は進める。
+    std::string Lexer::popRestStr(){
+        int tmp = idx;
+        idx = str.length();
+        return &str[tmp];
+    }
+
+    // 前回返したトークンの位置情報を文字列で返す
+    const char *Lexer::prePosStr() const{
+        static char buf[512];
+        sprintf(buf,"%s:%d:%d",file_name.c_str(), currentLineNo(), preColumnNo() );
+        return buf;
+    }
+
+
+    // 行末を指している? == その行で読める文字はもうない?
+    bool Lexer::eol() const{ return ((str.length()-idx) <= 0); }
+    // データをすべて読み終わった?
+    bool Lexer::eod() const{ return (is == NULL && eol()); }
+    // str を更新。
+    // idx が行末まで来た
+    //   -> 次の行を読む
+    //      行末に \ があれば、次の行と連結
+    // @return データをすべて読み終わっていたら false
+    bool Lexer::updateString(){
+        // 行末じゃない -> このままでOK
+        if( !eol() ) return true;
+        // 行末なので getline
+        idx = 0; pre_idx = 0;
+        if( line_no > 0 ) start_col_pos = col_offset; // 最初の getline では更新しない。
+        line_no++;
+        if( is == NULL || !getline(*is, str) ){
+            // 次の行がない -> false
+            is = NULL;
+            str = "";
+            return false;
+        }
+        // 行末に '\' -> 次の行と繋ぐ
+        while( re_lastEscape.match(str) ){
+            str = str.substr(0,str.length()-1);
+            std::string tmp;
+            line_no++;
+            if( !getline(*is, tmp) ){ is = NULL; break; }
+            str = str + tmp;
+        }
+        return true;
+    }
+
+    // カーソルを n 文字分進める
+    void Lexer::incCursor(unsigned int n){
+        pre_idx = idx;
+        idx += n;
+    }
+  
+	
 }; // namespace nl;
 
 
 #ifdef TEST
 // test
-// $ make TEST=lexer
+// $ make TEST=lexer_basic
 #include "util.h"
 
 using nl::RegEx;
@@ -33,11 +177,10 @@ using nl::Lexer;
 using nl::LexRule;
 using nl::LexRuleList;
 
-void showAllToken(Lexer &lexer){
+void showAllToken(Lexer &lexer,LexRuleList &rule_list){
     LexRule *ret;
-    DBGP("---------");
-    while( (ret = lexer.get()) != NULL ){
-        std::cout << "[" << lexer.getPosStr() << "] " << ret->type << "  '" << ret->str() << "'" << std::endl;
+    while( (ret = lexer.getToken(rule_list)) != NULL ){
+        std::cout << "[" << lexer.prePosStr() << "] " << "  '" << ret->str() << "'" << std::endl;
     }
     if(!lexer.eod()) DBGP("parse error.");
 }
@@ -47,23 +190,37 @@ int main(){
     LexRuleList rule_list;
 
     // タイプと正規表現のリストを作成
-    rule_list.push_back( LexRule(  0, "(^\\s+)|(^$)")); // whitespace | 空行
-    rule_list.push_back( LexRule(  1, "^\\w+"       )); // リテラル
-    rule_list.push_back( LexRule(  2, "^\\d+"       )); // 数値
-    rule_list.push_back( LexRule(  3, "^\"(((\\\\.)|[^\"])*)\"", 1)); // 文字列
-    rule_list.push_back( LexRule(  4, "^//.*$"      )); // コメント行
-    rule_list.push_back( LexRule(  5, "^[\xC0-\xD6]")); // 0xC0 - 0xD6 の一文字にマッチ
-    rule_list.push_back( LexRule(100, "^.*$"        )); // その他
-    // lexer にセット
-    lexer.setRule(&rule_list);
+    // rule_list.push_back( LexRule(  0, "(^\\s+)|(^$)")); // whitespace | 空行
+    // rule_list.push_back( LexRule(  1, "^\\w+"       )); // リテラル
+    // rule_list.push_back( LexRule(  2, "^\\d+"       )); // 数値
+    // rule_list.push_back( LexRule(  3, "^\"(((\\\\.)|[^\"])*)\"", 1)); // 文字列
+    // rule_list.push_back( LexRule(  4, "^//.*$"      )); // コメント行
+    // rule_list.push_back( LexRule(  5, "^[\xC0-\xD6]")); // 0xC0 - 0xD6 の一文字にマッチ
+    // rule_list.push_back( LexRule(100, "^.*$"        )); // その他
+    rule_list.push_back( LexRule("(^\\s+)|(^$)")); // whitespace | 空行
+    rule_list.push_back( LexRule("^\\w+"       )); // リテラル
+    rule_list.push_back( LexRule("^\\d+"       )); // 数値
+    rule_list.push_back( LexRule("^\"(((\\\\.)|[^\"])*)\"", 1)); // 文字列
+    rule_list.push_back( LexRule("^//.*$"      )); // コメント行
+    rule_list.push_back( LexRule("^[\xC0-\xD6]")); // 0xC0 - 0xD6 の一文字にマッチ
+    rule_list.push_back( LexRule("^.*$"        )); // その他
     
     // ファイル開く
+    DBGP("------------------------");
     lexer.setSourceFile("TestData/input.txt");
-    showAllToken(lexer);
+    showAllToken(lexer, rule_list);
+
     // 文字列をパース
+    DBGP("------------------------");
     lexer.setSourceText("// comment line.\nint a=0;\nint b=0;");
     lexer.setSourceInfo("text01", 3, 10, 20);
-    showAllToken(lexer);
+    showAllToken(lexer, rule_list);
+
+    //
+    DBGP("------------------------");
+    lexer.setSourceFile("TestData/simple.xml");
+    lexer.getToken("<?xml");
+    DBGP(lexer.popRestStr());
 
     return 0;
 }
--- lexer.h
+++ lexer.h
@@ -1,59 +1,150 @@
 // -*- mode: cpp -*-
 // 字句解析用
 //  regex を利用。 -lregex
-//  BasicLexer を継承した Lexer クラス
 // class:
 //   - Lexer
-#ifndef __NOMLIB_LEXER_H__
-#define __NOMLIB_LEXER_H__
+//
+//   - LexRule
+//   - LexRuleList  <- typedef
+#ifndef __NOMLIB_LEXER_BASIC_H__
+#define __NOMLIB_LEXER_BASIC_H__
 
 #include <string>
 #include <list>
 #include <sstream> // string stream
 #include <fstream> // file stream
 
-#include "lexer_basic.h"
+#include "regex.h"
 
 namespace nl{
-    class Lexer : public BasicLexer{
-    public:
-        virtual ~Lexer();
-    };
-#if 0
     class Lexer;
+    // クラスの宣言 中身は Lexer のあと
+    class LexRule;  // 切り出しのルール
+    typedef std::list<LexRule> LexRuleList;
     
-    // 登録されたルールに従って、入力ファイル/文字列 からトークンを切り出す
-    class Lexer : public BasicLexer{
+    // 入力ファイル/文字列 からトークンを切り出す
+    class Lexer{
     public:
+        Lexer();
+        explicit Lexer(const std::string &file_name_);
         virtual ~Lexer();
-        
-        // token を返す。
-        LexRule *getToken();
-        
-        // 切り出した token が条件(type/str())にマッチしていたら、その token を返す。
-        // マッチしてなかったら unget して NULL を返す。
-        // std::list で複数の条件を指定可(or)。
+        Lexer(const Lexer &); // コピーコンストラクタ 元のやつは壊す
+    private:
+        Lexer &operator=(Lexer &){ return *this; } // 代入演算子
+    public:
+        /// 入力元の設定
+        void setSourceFile(const std::string &file_name_);
+        void setSourceText(const std::string &content_);
+        // 入力元の追加情報のセット
+        // file_name_ は、 "" を渡すと file_name を更新しない。
+        void setSourceInfo(const std::string &file_name_,
+                           const int line_offs = 0,        // line の初期位置
+                           const int col_offs  = 0,        // col のオフセット
+                           const int start_col_pos_  = 0); // col の初期位置
+
+        /// トークンを切り出す。
+        // @return マッチした: true / マッチしなかった: false
+        bool getToken(LexRule &tkn);
+        bool getToken(const std::string &tkn_);
+        // @return 最初にマッチした token へのポインタ or NULL
         template<class T>
-        LexRule *getToken(const T &val){
-            if( !getToken() ) return NULL;
-            if( reg_tkn->equals(val) ) return reg_tkn;
-            unget();
+        T *getToken(std::list<T> &lst){
+            for( std::list<LexRule>::iterator ite = lst.begin(); ite != lst.end(); ++ite){
+                if( getToken(*ite) ) return &(*ite);
+            }
             return NULL;
         }
-        
         // getToken で前回返した token 文字列
-        std::string reg(){ return reg_tkn ? reg_tkn->str() : ""; }
+        std::string reg() const{ return reg_str; }
         
-        // 条件にマッチする token を読み飛ばす。
+        /// 前回の get() を無かったことにする
+        void unget();
+
+        /// 条件にマッチする token を読み飛ばす。
+        // T: LexRule, string, std::list<LexRule>, std::list<string>
         template<class T>
-        void skipToken(const T &val){
-            while( getToken(val) );
-        }
+        void skipToken(T &val){ while( getToken(val) ); }
+
+        /// 前回返したトークンの情報を返す
+        const char *prePosStr() const; // 位置情報 を文字列で返す
+
+        bool eod() const; // データをすべて読み終わった?
+    private:
+        // 読み取り中の行を返す。idx は進めない。
+        const std::string &currentLine() const{ return str; }
+        std::string popRestStr();  // 読み取り中の行の残り部分を返す。idx は進める。
+        
+        // 現在の行/列番号の取得
+        int currentLineNo()       const{ return line_no   + line_offset;      }
+        int currentColumnNo() const{ return idx    +1 + start_col_pos;        } // 列番号は 1 から始まる
+        int preColumnNo()     const{ return pre_idx+1 + start_col_pos; } // 前回返したトークンの開始位置
+        const std::string &fileName() const{ return file_name; }
+	
+        bool eol() const; // 行末を指している? == その行で読める文字はもうない?
         
     private:
-        LexRule *reg_tkn; // getToken で前回返した token
+        void init(); // 初期化
+        // str を更新。
+        // idx が行末まで来た
+        //   -> 次の行を読む
+        //      行末に \ があれば、次の行と接続。
+        // @return データをすべて読み終わっていたら false
+        bool updateString();
+        // カーソルを n 文字分進める
+        void incCursor(unsigned int n);
+	
+    private:
+        // File の内容
+        std::string str; // カーソルがある行の文字列
+        // カーソル
+        int line_no; // 現在の str が何行目のものか
+        int idx;     // str の何文字目までスキャンしたか
+        int pre_idx; // 前回 get() が呼ばれたときの idx (unget用)
+
+        std::string reg_str; // getToken で前回返した token
+        
+        // File にあたる部分
+        std::string file_name;
+        std::istream  *is; // ポインタ. ifs / ss を指す
+        mutable std::ifstream *ifs;
+        mutable std::stringstream *ss;
+	
+        int line_offset;   // オフセット
+        int col_offset;    // 
+        int start_col_pos; // col の初期位置
     };
-#endif
+  
+    // 字句解析ルール
+    //  正規表現で表す。
+    //  ユーザ定義の type は正の値。
+    class LexRule : public RegEx{
+    public:
+        // @param type_  識別用
+        // @param rxstr  正規表現 文字列
+        // @param idx    何番目のマッチを str() で返すか
+        LexRule(unsigned int type_, const std::string &rxstr, unsigned int idx_ = 0) : RegEx(rxstr), type(type_), idx(idx_){}
+        LexRule(const std::string &rxstr, unsigned int idx_ = 0) : RegEx(rxstr), type(0), idx(idx_){}
+        ~LexRule(){}
+        LexRule(const LexRule &obj) : RegEx(obj), type(obj.type), idx(obj.idx){} // コピーコンストラクタ
+
+        // type / str() が等しい?
+        bool operator==(const unsigned int &type_) const{ return type == type_; }
+        bool operator==(const std::string  &str_ ) const{ return str() == str_; }
+        template<class T>
+        bool equals(const T &val) const{ return operator==(val); }
+        // list で type / str() を指定
+        template<class T>
+        bool equals(const std::list<T> &vals) const{
+            return ( std::find(vals.begin(), vals.end(), *this) != vals.end() );
+        }
+    private:
+        LexRule &operator=(LexRule &){ return *this; }    // 代入演算子 禁止
+    public:
+        std::string str() const{ return ( idx<length() )?get(idx):""; } // マッチ部分の文字列
+    public:
+        const unsigned int type;
+        const unsigned int idx;
+    };
   
 }; // namespace nl
 
